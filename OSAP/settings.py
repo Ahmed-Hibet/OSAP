622f9a8840ae3bde68b54744ac7003b975110037
Ahmed Hibet <ahmedhibet@gmail.com>
2022-06-13 07:22:12 +0300

    added STATIC_ROOT

--- OSAP/settings.py
+++ OSAP/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -137,6 +138,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # the image files storage location
+MEDIA_URL = '/media/' # a URL to access media files through a browser
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
